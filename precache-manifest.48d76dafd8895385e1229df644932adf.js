856b259e76f2373f892e0c18a043d61b3b4b0868
Kowsalya G <Kowsalya-6100@csez.zohocorpin.com>
2019-12-19 16:39:51 +0530

    Updates

---
+++ precache-manifest.48d76dafd8895385e1229df644932adf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca04ece773933cf6c64be26fc30408b1",
+    "url": "/YouTubeCloned/index.html"
+  },
+  {
+    "revision": "e0892aa8ae9c835d7a79",
+    "url": "/YouTubeCloned/static/js/2.89de22ce.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/YouTubeCloned/static/js/2.89de22ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "172e766ff2a64c04468a",
+    "url": "/YouTubeCloned/static/js/main.9c267f6b.chunk.js"
+  },
+  {
+    "revision": "3eb317c64e6b8ebdd406",
+    "url": "/YouTubeCloned/static/js/runtime-main.554f24c1.js"
+  }
+]);
