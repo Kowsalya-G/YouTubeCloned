155216240799932ed28ac0185d807be9064b8089
Kowsalya G <Kowsalya-6100@csez.zohocorpin.com>
2019-12-26 18:58:21 +0530

    Updates

---
+++ precache-manifest.0412fdeec1d1bf7f3630350ec5485b26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2bad326e76cc97f8826195acfe7a8f8",
+    "url": "/YouTubeCloned/index.html"
+  },
+  {
+    "revision": "e0892aa8ae9c835d7a79",
+    "url": "/YouTubeCloned/static/js/2.89de22ce.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/YouTubeCloned/static/js/2.89de22ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b1d4febb133962df7c5",
+    "url": "/YouTubeCloned/static/js/main.eaf611f6.chunk.js"
+  },
+  {
+    "revision": "3eb317c64e6b8ebdd406",
+    "url": "/YouTubeCloned/static/js/runtime-main.554f24c1.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SearchBar } from './SeacrhBar';
-export { default as VideoDetail } from './VideoDetail';
-export { default as VideoList } from './VideoList';
-// export { default as SearchBar } from './SeacrhBar';
